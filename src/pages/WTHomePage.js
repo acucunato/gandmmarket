34c3cf86d5a6f82dd08eab949e5de07428ac0c26
Alyssa Cucunato <acucunato18@gmail.com>
2021-05-23 09:25:30 -0400

    updated menu, added contact section on WT page

--- src/pages/WTHomePage.js
+++ src/pages/WTHomePage.js
@@ -158,6 +158,58 @@ function WTHomePage() {
           </table>
         </table>
       </div>
+      <br />
+      <br />
+      {/* contact us  */}
+      <section className="page-section-contact" id="contact">
+        <h2 className="text-center mt-0 titleText wow fadeIn delay-1s">
+          Contact Us
+        </h2>
+        <div class="horizontal-line"></div>
+        <div className="container">
+          {/* WASHINGTON TWP */}
+
+          <div className="row">
+            <div className="col-md-4 text-center wow flipInX delay-1s">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3068.2599232991115!2d-75.06599548483096!3d39.73381187945041!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6d6a7cb9ee91b%3A0x67f2fd24bd085811!2s421%20Hurffville%20-%20Cross%20Keys%20Rd%2C%20Sewell%2C%20NJ%2008080!5e0!3m2!1sen!2sus!4v1620746822773!5m2!1sen!2sus"
+                width="400"
+                height="300"
+                frameborder="0"
+                style={{ border: "0" }}
+                allowfullscreen=""
+                aria-hidden="false"
+                tabindex="0"
+                title="maptogm"
+                id="map"
+              ></iframe>
+            </div>
+            <div className="col-md-4 text-center wow flipInX delay-2s">
+              <ul>
+                <h4 className="hours">Our Hours</h4>
+                <li>Monday 10am-6pm</li>
+                <li>Tuesday 10am-6pm</li>
+                <li>Wednesday 10am-6pm</li>
+                <li>Thursday 10am-6pm</li>
+                <li>Friday 10am-6pm</li>
+                <li>Saturday 10am-6pm</li>
+                <li>Sunday 9am-3pm</li>
+                <br />
+              </ul>
+            </div>
+            <div className="col-md-4 wow flipInX delay-3s">
+              <ul>
+                <li>
+                  <i className="fas fa-phone-square fa-2x"></i>
+                  <a href="tel:+1-856-939-1070">
+                    <b>coming soon</b>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
